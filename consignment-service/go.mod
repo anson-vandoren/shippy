b73dd11d94ea00e9b6590a47a9ab79d66a5d3efa
Anson VanDoren <anson.vandoren@gmail.com>
2019-11-28 09:17:17 -0800

    initial commit

---
+++ consignment-service/go.mod
@@ -0,0 +1,12 @@
+module github.com/anson-vandoren/shippy/consignment-service
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.3.2
+	github.com/micro/go-micro v1.17.1
+	golang.org/x/net v0.0.0-20191126235420-ef20fe5d7933
+	golang.org/x/sys v0.0.0-20191128015809-6d18c012aee9 // indirect
+	google.golang.org/genproto v0.0.0-20191115221424-83cc0476cb11 // indirect
+	google.golang.org/grpc v1.25.1
+)
---
+++ consignment-service/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+
+	"context"
+
+	pb "github.com/anson-vandoren/shippy/consignment-service/proto/consignment"
+	"github.com/micro/go-micro"
+)
+
+type repository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
+}
+
+type Repository struct {
+	mu           sync.RWMutex
+	consignments []*pb.Consignment
+}
+
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	repo.mu.Lock()
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	repo.mu.Unlock()
+	return consignment, nil
+}
+
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
+type service struct {
+	repo repository
+}
+
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return err
+	}
+
+	res.Created = true
+	res.Consignment = consignment
+	return nil
+}
+
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
+	consignments := s.repo.GetAll()
+	res.Consignments = consignments
+	return nil
+}
+
+func main() {
+
+	repo := &Repository{}
+
+	// Create a new service.
+	srv := micro.NewService(
+		// This name must match the package name given in the protobuf definition
+		micro.Name("shippy.service.consignment"),
+	)
+
+	srv.Init()
+
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+
+	if err := srv.Run(); err != nil {
+		fmt.Println(err)
+	}
+
+}
